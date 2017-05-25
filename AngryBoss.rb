265ae2732bde78bdbf9206e9bee631bea51631e0
Tom Grudzinski <tgbiz@cox.net>
2017-05-25 14:59:40 -0500

    first commit

---
+++ AngryBoss.rb
@@ -0,0 +1,4 @@
+puts 'Hello there, Mr. Bossman...'
+puts 'May I please have a raise?'
+puts ''
+puts 'WHADDYA MEAN...I WANT A RAISE?!?  YOU\'RE FIRED!!!'
---
+++ OldSchoolRomanNumerals.rb
@@ -0,0 +1,17 @@
+def old_roman_numeral num
+  raise 'Must use positive integer' if num <= 0
+  roman = ''
+  
+  roman << 'M' * (num        /1000)
+  roman << 'D' * (num % 1000 / 500)
+  roman << 'C' * (num %  500 / 100)
+  roman << 'L' * (num %  100 /  50)
+  roman << 'X' * (num %   50 /  10)
+  roman << 'V' * (num %   10 /   5)
+  roman << 'I' * (num %    5 /   1)
+  
+  roman
+end
+puts 'What year would you like to convert to Old Roman Numerals?'
+year = gets.chomp.to_i
+puts(old_roman_numeral(year))
